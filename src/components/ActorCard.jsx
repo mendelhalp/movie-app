a88200db5c71b05363ad597e791ff87de1c7e7e9
Menachem Halperin <mendelhalp@gmail.com>
2020-12-29 23:00:01 +0200

    design the links to IMDb

--- src/components/ActorCard.jsx
+++ src/components/ActorCard.jsx
@@ -1,18 +1,25 @@
-import { Card } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import './ActorCard.css';
 
-const ActorCard = ({actor}) => 
+const ActorCard = ({ actor }) =>
 
-        <div className="c-actor-card mb-4">
-            <Card>
-                <Card.Img variant="top" src={actor.img} />
-                <Card.Body className="p-2">
-                <a href={actor.link} target="_blank">
-                    <Card.Title className="text-center">{actor.fname + " " + actor.lname + " " + actor.age()}</Card.Title>
-                </a>
-                </Card.Body>
-            </Card>
-        </div>
+    <div className="c-actor-card mb-4">
+        <Card>
+            <Card.Img variant="top" src={actor.img} />
+            <Card.Body className="p-2">
+                <Row className="justify-content-between">
+                    <Col xs={'auto'}>
+                        <a href={actor.link} target="_blank">
+                            <Card.Text className="text-left font-weight-bold">{actor.fname + " " + actor.lname}</Card.Text>
+                        </a>
+                    </Col>
+                    <Col xs={'auto'}>
+                        <Card.Text className="text-right">{actor.age()}</Card.Text>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    </div>
 
 
 
