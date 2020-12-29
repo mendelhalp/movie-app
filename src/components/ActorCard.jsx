575c6e24f12e48251c5a9bbc85d3f40a0eff2930
Menachem Halperin <mendelhalp@gmail.com>
2020-12-29 16:10:34 +0200

    render ActorCard in CtorsPage

--- src/components/ActorCard.jsx
+++ src/components/ActorCard.jsx
@@ -1,11 +1,19 @@
+import { Card } from 'react-bootstrap';
 import './ActorCard.css';
 
 const ActorCard = (props) => {
-    
+    const { fname, lname, age, imgSrc, imdbLink } = props;
 
     return (
-        <div>
-
+        <div className="c-actor-card">
+            <Card>
+                <a href={imdbLink} target="_blank">
+                    <Card.Img variant="top" src={imgSrc} />
+                </a>
+                <Card.Body>
+                    <Card.Title className="text-center">{fname + " " + lname + " " + age}</Card.Title>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
--- src/pages/ActorsPage.jsx
+++ src/pages/ActorsPage.jsx
@@ -1,6 +1,7 @@
 import './ActorsPage.css';
 import Actor from '../model/ActorModel';
 import ActorCard from '../components/ActorCard';
+import { Col, Container, Row } from 'react-bootstrap';
 
 
 let actorsArr = [];
@@ -10,13 +11,23 @@ actorsArr.push(new Actor("Louis", "de Funès", "1914-07-31", "https://m.media-am
 actorsArr.push(new Actor("Suzy", "Delair", "1917-12-31", "https://m.media-amazon.com/images/M/MV5BMTQ0MjgwMjEyMV5BMl5BanBnXkFtZTgwMjExNDc3NTE@._V1_UY317_CR132,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0216177/?ref_=tt_cl_t2"));
 actorsArr.push(new Actor("Janet", "Brandt", "1914-12-13", "https://m.media-amazon.com/images/M/MV5BZGJkOWQyY2MtOGQ4Mi00MjY0LTkwM2EtNWMwMTEzYzg4YTFkXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_UY317_CR9,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0104935/?ref_=tt_cl_t6"));
 
+
 const ActorsPage = () => {
 
+    const cardColumns = actorsArr.map(actor =>
 
+        <Col xs={12} md={6} lg={3}>
+            <ActorCard fname={actor.fname} lname={actor.lname} age={actor.age()} imgSrc={actor.imgSrc} imdbLink={actor.imdbLink} />
+        </Col>
+    );
 
     return (
-        <div>
-            <ActorCard/>
+        <div className="p-actor">
+            <Container>
+                <Row>
+                    {cardColumns}
+                </Row>
+            </Container>
         </div>
     )
 }
