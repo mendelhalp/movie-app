fbba5f8275c3f7775466f80f9e4416861127f7f3
Menachem Halperin <mendelhalp@gmail.com>
2021-01-03 15:33:53 +0200

    add routing + HomePage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import { Container, Nav } from 'react-bootstrap';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import ActorsPage from './pages/ActorsPage';
+import HomePage from './pages/HomePage';
 import MoviesPage from './pages/MoviesPage';
 import TmdbMoviesPage from './pages/tmdbMoviesPage';
 
 function App() {
   return (
     <>
-      <ActorsPage/>
-      {/* <MoviesPage/> */}
-      <TmdbMoviesPage/>
+      <HashRouter>
+        <Container>
+          <Nav variant="tabs" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link href="#/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#/actors">Actors</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#/movies">Movies</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
+
+        <Switch>
+          <Route exact path="/"><HomePage /></Route>
+          <Route exact path="/home"><HomePage /></Route>
+          <Route exact path="/actors"><ActorsPage /></Route>
+          <Route exact path="/movies"><TmdbMoviesPage /></Route>
+        </Switch>
+      </HashRouter>
     </>
   );
 }
